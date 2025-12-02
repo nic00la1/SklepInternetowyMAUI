56254096876aa2ea1917454ecf0835db39aceab5
nic00la1 <NicolaKaleta@TE-LOD-D-06S209>
2025-12-02 12:02:55 +0100

    Działa przycisk zakup

--- SklepInternetowyMAUI/Classes/Produkt.cs
+++ SklepInternetowyMAUI/Classes/Produkt.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace SklepInternetowyMAUI.Classes
 {
-    public class Produkt
+    public class Produkt : INotifyPropertyChanged
     {
         public int Id { get; set; }
         public string Nazwa { get; set; }
         public string Obrazek { get; set; }
         public decimal Cena { get; set; }
         public string Kategoria { get; set; }
-        public int DostepnaIlosc { get; set; }
+
+        private int dostepnaIlosc;
+        public int DostepnaIlosc
+        {
+            get => dostepnaIlosc;
+            set
+            {
+                if (dostepnaIlosc != value)
+                {
+                    dostepnaIlosc = value;
+                    OnPropertyChanged(nameof(DostepnaIlosc));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
