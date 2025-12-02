9b275cad5c78810b9ecd80d2716ea666b0636b14
nic00la1 <NicolaKaleta@TE-LOD-D-06S209>
2025-12-02 09:11:50 +0100

    Dodaj pliki projektów.

---
+++ SklepInternetowyMAUI/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SklepInternetowyMAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "SklepInternetowyMAUI.Pages")]
