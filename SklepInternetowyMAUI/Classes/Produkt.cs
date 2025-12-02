b032f103b25405862add717cc59146ac80f76798
nic00la1 <NicolaKaleta@TE-LOD-D-06S209>
2025-12-02 11:03:26 +0100

    utworzono klasę Produkt

---
+++ SklepInternetowyMAUI/Classes/Produkt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepInternetowyMAUI.Classes
+{
+    public class Produkt
+    {
+        public int Id { get; set; }
+        public string Nazwa { get; set; }
+        public string Obrazek { get; set; }
+        public decimal Cena { get; set; }
+        public string Kategoria { get; set; }
+        public int DostepnaIlosc { get; set; }
+    }
+}
