6ab13e55f7b729bff6d25f67e77d803ea7f764f3
nic00la1 <NicolaKaleta@TE-LOD-D-06S209>
2025-12-02 11:15:30 +0100

    pobranie z c#

--- SklepInternetowyMAUI/MainPage.xaml.cs
+++ SklepInternetowyMAUI/MainPage.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace SklepInternetowyMAUI
+﻿using SklepInternetowyMAUI.Classes;
+
+namespace SklepInternetowyMAUI
 {
     public partial class MainPage : ContentPage
     {
+        public List<Produkt> Produkty { get; set; }
         public MainPage()
         {
             InitializeComponent();
+
+            var lines = File.ReadAllLines("data_store.txt");
+            Produkty = lines.Skip(1) // pomija nagłówek
+                .Select(line =>
+                {
+                    var parts = line.Split(';');
+                    return new Produkt
+                    {
+                        Id = int.Parse(parts[0]),
+                        Nazwa = parts[1],
+                        Obrazek = parts[2],
+                        Cena = decimal.Parse(parts[3]),
+                        Kategoria = parts[4],
+                        DostepnaIlosc = int.Parse(parts[5]),
+                    };
+                })
+                .ToList();
+
+            BindingContext = this;
         }
     }
 }
